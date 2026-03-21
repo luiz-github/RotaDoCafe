fe0cf70786867363684ea0d4db3eccf69bed0d5d
alysson <alysson.silva364@gmail.com>
2026-03-21 14:22:22 -0300

    :sparkles: feat: mocked user admin

--- RotasDoCafe/src/hooks/AuthScreen/useLogin.js
+++ RotasDoCafe/src/hooks/AuthScreen/useLogin.js
@@ -1,14 +1,27 @@
 import { Alert } from "react-native";
-export default function useLogout(navigation) {
-    const handleLogout = (username, passowrd) => {
+export default function useLogin(navigation) {
 
-        if (!username || !passowrd) {
+    const MOCK_USER = {
+        username: "admin",
+        password: "123"
+    }
+    const handleLogin = (username, password) => {
+
+        if (!username || !password) {
             Alert.alert("Erro", "Preencha todos os campos para continuar.");
             return;
         }
 
-        navigation.replace("App");
+        if (
+            username === MOCK_USER.username &&
+            password === MOCK_USER.password
+        ) {
+            Alert.alert("Sucesso", `Bem-vindo, ${MOCK_USER.username}!`);
+            navigation.replace("App");
+            return;
+        }
 
+        Alert.alert("Erro", "Usuário ou senha inválidos.");
     };
-    return handleLogout;
+    return handleLogin;
 }
--- RotasDoCafe/src/hooks/AuthScreen/useLoginForm.js
+++ RotasDoCafe/src/hooks/AuthScreen/useLoginForm.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function useLoginForm() {
 
@@ -6,11 +6,12 @@ export default function useLoginForm() {
     const [password, setPassword] = useState("");
 
 
-    return(
+    return {
         username,
         password,
         setUsername,
         setPassword
-    )
+    }
+
 
 }
